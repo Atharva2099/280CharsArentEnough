cf9db44fb303cef037e3ded66754c628578cf6bf
Atharva2099 <93972767+Atharva2099@users.noreply.github.com>
2024-11-07 20:58:36 -0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>What is he yapping?</title>
+        <title>Atharv unclogged</title>
         <link 
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
           rel="stylesheet"
